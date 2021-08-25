58d46d1f5ba3b4c5a8f785127c6d229728a24be9
Itallodavid <itallodavid.fdasilva@gmail.com>
2021-08-24 22:48:32 -0300

    Add method create FakeUpdate DTO and change fields to public

--- src/test/java/itallodavid/github/personapi/utils/PersonUtils.java
+++ src/test/java/itallodavid/github/personapi/utils/PersonUtils.java
@@ -1,6 +1,7 @@
 package itallodavid.github.personapi.utils;
 
 import itallodavid.github.personapi.dto.PersonDTO;
+import itallodavid.github.personapi.dto.PersonUpdateDTO;
 import itallodavid.github.personapi.model.Person;
 
 import java.time.LocalDate;
@@ -8,10 +9,10 @@ import java.time.format.DateTimeFormatter;
 
 public class PersonUtils {
 
-    private static final String FIRST_NAME = "Itallo";
-    private static final String LAST_NAME = "David";
-    private static final String CPF_NUMBER = "45815102016";
-    private static final LocalDate BIRTH_DATE = LocalDate.of(2021, 1, 1);
+    public static final String FIRST_NAME = "Itallo";
+    public static final String LAST_NAME = "David";
+    public static final String CPF_NUMBER = "45815102016";
+    public static final LocalDate BIRTH_DATE = LocalDate.of(2021, 1, 1);
 
     public static PersonDTO createFakeDTOWithoutId(){
         return PersonDTO.builder()
@@ -44,6 +45,14 @@ public class PersonUtils {
             .firstName(FIRST_NAME)
             .lastName(LAST_NAME)
             .birthDate(BIRTH_DATE)
+            .build();
+    }
+
+    public static PersonUpdateDTO createFakeUpdateDTO(){
+        return PersonUpdateDTO.builder()
+            .firstName(FIRST_NAME)
+            .lastName(LAST_NAME)
+            .birthDate(BIRTH_DATE.format(DateTimeFormatter.ofPattern("dd-MM-yyy")))
             .build();
     }
 }
